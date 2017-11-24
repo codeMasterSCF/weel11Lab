fe02b6e2e89777b28ca926e417c8e8e00c55fea9
463849 <463849@ELF6IT215756.ACDM.DS.SAIT.CA>
2017-11-23 18:08:09 -0700

    Initialized Repository created project

---
+++ src/java/businesslogic/AccountService.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businesslogic;
+
+import dataaccess.NotesDBException;
+import dataaccess.UserDB;
+import domainmodel.*;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+import javax.naming.NamingException;
+
+/**
+ *
+ * @author awarsyle
+ */
+public class AccountService {
+    
+    public User checkLogin(String username, String password, String path) {
+        User user;
+        
+        UserDB userDB = new UserDB();
+        try {
+            user = userDB.getUser(username);
+            
+            if (user.getPassword().equals(password)) {
+                Logger.getLogger(AccountService.class.getName())
+                        .log(Level.INFO, 
+                        "A user logged in: {0}",
+                        username);
+                try {
+                    //WebMailService.sendMail(user.getEmail(), "NotesKeepr Logged in", "<h2>Congrats!  You just loggedin successfully.</h2>" , true);
+                    
+                    HashMap<String, String> contents = new HashMap<>();
+                    
+                    contents.put("firstname", user.getFirstname());
+                    contents.put("date", (new java.util.Date()).toString());
+                    
+                    String template = path + "/emailtemplates/login.html";
+                    WebMailService.sendMail(user.getEmail(), "NotesKeepr Login", template, contents);
+                    
+                } catch (Exception ex) {
+                    Logger.getLogger(AccountService.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                return user;
+            }
+            
+        } catch (NotesDBException ex) {
+        }
+        
+        return null;
+    }
+    
+}
